3ca4262b46a72c2b92add88ec7ac567f2946200b
bewa4021 <bewa4021@colorado.edu>
2018-04-19 18:09:17 +0000

    fixed URLS at the end of tweets not being removed\made Parser a callable function.

--- parser/csvParser.py
+++ parser/csvParser.py
@@ -2,34 +2,36 @@ import csv
 import io
 import re
 
-ifile = open('FullCorpus.csv', "rt")
-#ifile = open('FullCorpus.csv', "rt", encoding = "utf8")
+def Parse():
+    ifile = open('FullCorpus.csv', "rt")
+    #ifile = open('FullCorpus.csv', "rt", encoding = "utf8")
 
-reader = csv.reader(ifile)
+    reader = csv.reader(ifile)
 
-sentiments = []
-tweets = []
+    sentiments = []
+    tweets = []
 
-rownum = 0
-for row in reader:
-    if(rownum == 0):
-        header = row
-    else:
-        colnum = 0
-        for col in row:
-            if(header[colnum] == "Sentiment"):
-                sentiments.append(col)
-            if(header[colnum] == "TweetText"):
-                URLlessTweet = re.sub(r'https?:\/\/.*\s|\r', '', col)#, flags=re.MULTILINE)
-                tweets.append(URLlessTweet)
-            colnum += 1
-    rownum += 1
-ifile.close()
+    rownum = 0
+    for row in reader:
+        if(rownum == 0):
+            header = row
+        else:
+            colnum = 0
+            for col in row:
+                if(header[colnum] == "Sentiment"):
+                    sentiments.append(col)
+                if(header[colnum] == "TweetText"):
+                    URLlessTweet = re.sub(r'https?:\/\/.*\s|\r', '', col)#, flags=re.MULTILINE)
+                    URLlessTweet = re.sub(r'https?:\/\/.*$', '', URLlessTweet)#, flags=re.MULTILINE)
+                    tweets.append(URLlessTweet)
+                colnum += 1
+        rownum += 1
+    ifile.close()
+    return sentiments,tweets
 
+sentiments,tweets = Parse()
 for ii in range(100):
     print"Senitment: ",sentiments[ii],", Tweet: ",tweets[ii]
 
-print tweets[6]
-URLlessTweet = re.sub(r'https?:\/\/.*\s', '', tweets[6], flags=re.MULTILINE)
-print URLlessTweet
+
 #print tweets[6].split()
