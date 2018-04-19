dcf55c99b3c2036e8feada8b2ed7fefce7e6cd67
eric-ha <eric.ha006@gmail.com>
2018-04-19 17:27:13 -0600

    Footer added

--- index.php
+++ index.php
@@ -163,6 +163,33 @@ of the tweet.</p>
     </ul>
   </div>
   <hr>
+  <div>
+    <head>
+      <title>Basic Footer</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+      <link rel="stylesheet" href="assets/css/Footer-white.css">
+    </head>
+
+    <body>
+        <div class="content">
+  </div>
+      <footer id="myFooter">
+          <div class="container">
+              <ul>
+                  <li><a href="https://github.com/haydennix55/ClutterSight">Github</a></li>
+                  <li><a href="https://www.pivotaltracker.com/n/projects/2155039">Pivotal Tracker</a></li>
+                  <a href="https://twitter.com/bigdatacu?lang=en" class="social-icons"><i class="fa fa-twitter"></i></a>
+                </ul>
+                  <p class="footer-copyright">© ClutterSight 2018</p>
+              </div>
+      </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      </body>
+  </div>
 
 <!-- END Introduction Menu -->
 </div>
