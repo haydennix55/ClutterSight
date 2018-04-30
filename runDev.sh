aa54395fb143ca4f156cb6e82694d17aefa6fec8
hani2284 <hayden.nix@colorado.edu>
2018-04-30 09:26:48 -0600

    travis update, gitignore block pyc

--- runDev.sh
+++ runDev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn compile --quiet exec:java -Dexec.classpathScope=compile -Dexec.mainClass=storm.ClutterSightTopology -Dexec.args="dev" -Dexec.cleanupDaemonThreads=false
+mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=storm.ClutterSightTopology -Dexec.args="dev" -Dexec.cleanupDaemonThreads=false
 python PythonML/db_access.py dev
